bb5575b37c52f363af6e46b8f7a4b214f7460e59
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-09-30 19:24:25 +1000

    Singly Linked Lists: Creation and Traversal.

--- Makefile
+++ Makefile
@@ -1,4 +1,15 @@
-CFLAGS=-Wall -g
+CFLAGS = -Wall -g
 
+# Rule to build the executable "main" from linked_list.c
+# - The $@ is the target (main), and $^ is the dependencies (linked_list.o)
+main: linked_lists.o
+	gcc $(CFLAGS) -o $@ $^
+
+# Rule to build the object file linked_list.o from linked_list.c
+# - This compiles the .c file into an object file
+linked_list.o: linked_lists.c
+	gcc $(CFLAGS) -c $<
+
+# Clean rule to remove generated files
 clean:
-	rm -f main
+	rm -f arrays linked_lists main linked_lists.o
--- linked_lists.c
+++ linked_lists.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
  * Like arrays, linked lists also store items.
@@ -18,6 +19,8 @@
  * In arrays, direct-access only. In Linked Lists, sequential-access only, including the last node.
  * Though, insertion and deletion is easier.
  */
+
+/**********************************************************************************************/
 
 /*
  * ***Types of Linked Lists***
@@ -58,8 +61,96 @@
  * - First node's prev pointer stores address of last node
  */
 
+/**********************************************************************************************/
 
 int main()
 {
-    printf("Linked Lists!\n");
+    creation_and_traversal();
+}
+
+/**********************************************************************************************/
+
+// Define node
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void creation_and_traversal()
+{
+    // Defining head
+    // head points to struct node, imagine int *p
+    // Initially, head is 0/null, does not store anything
+    struct node *head = NULL;
+
+    // To accomodate for having more than 2 nodes
+    // This is because we cannot move the *head or else would lose referencer to first node
+    // Traverses the list
+    struct node *temp;
+
+    // In this pointer we store every single new node that we create henceforth
+    struct node *newNode;
+
+    // The number of nodes the user inputs
+    int number_of_nodes;
+
+    // User input: number of nodes
+    printf("How many nodes do you want to insert? ");
+    scanf("%d", &number_of_nodes);
+
+    for (int i = 0; i < number_of_nodes; i++)
+    {
+        /**********************************************************************************************/
+
+        // Creating a new node
+        // malloc returns a pointer to the starting address of that memory block, AKA void pointer
+        // Thus, we need to type cast it so we can store the address it gives
+        // We store the pointer that malloc returns inside the newNode pointer
+        newNode = (struct node *)malloc(sizeof(struct node)); // 4 bytes + 4 bytes = 8 bytes
+
+        // User input for data
+        printf("Enter the data for node %d: ", i + 1);
+        // Cannot directly access members of a struct and use &data
+        // Read as: newNode's data part of the struct
+        scanf("%d", &newNode->data);
+
+        // Sets up the address part after the data part has been inserted
+        newNode->next = NULL;
+
+        /**********************************************************************************************/
+
+        // Now we need to put the newNode into the linkage
+        // We store the first node's address into head
+        // This condition ensures head is only set at the starting case
+        if (head == NULL)
+        {
+            head = temp = newNode;
+        }
+        else
+        {
+            // Currently, the last node's address part (next) is NULL
+            // So we update the last node's address part to hold this new node's address
+            // We link this last node to the new node by setting its next pointer to point to newNode
+            // This means that the current node (referred to by temp) will now have its next pointer updated to point to the new node you are adding (newNode)
+            // In this case, temp is a pointer to the last node
+            temp->next = newNode;
+            // temp is still pointing to the last node
+            // Updates temp to point to the new node
+            // In this case, temp itself is updated instead of the last node
+            temp = newNode;
+        }
+    }
+
+    // Now, traverse the linked list and print its data
+    printf("\nTraversing the linked list:\n");
+    temp = head; // Start from the head node
+    while (temp != NULL)
+    {
+        // Print the data of the current node
+        printf("%d -> ", temp->data);
+        // Move to the next node
+        temp = temp->next;
+    }
+    printf("NULL\n");
 }
