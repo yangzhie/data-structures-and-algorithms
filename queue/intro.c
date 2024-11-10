d94551a12cc435ca4809d6b7af011629215584b2
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-11-10 17:17:40 +1100

    Queue: introduction.

---
+++ queue/intro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
+
+/*
+ * Queue is a linear data structure which follows a particular order in which the operations are performed.
+ * Is an Abstract Data Type (ADT) that holds a collection of elements.
+ * First in first out (FIFO).
+ * 
+ * Insertion (Enqueue) is performed from the Rear/fail
+ * Deletion (Dequeue) is performed from the Front/head 
+ * 
+ * Applications:
+ * - CPU scheduling
+ * - Disk scheduling
+ * - Printer queue
+*/
