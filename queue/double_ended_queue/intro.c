fb57de11b2f76a7d7b937c6d4fe88a7f3c555518
ArnavMalasi <arnavmalasi2212@gmail.com>
2025-03-10 19:41:37 +1100

    DEQue: introduction.

---
+++ queue/double_ended_queue/intro.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+}
+
+// DEQueue - Double Ended Queue
+// Insertion and deletion can be done at both ends (front and rear)
+// Has properties of both stacks and queues, thus can be used as one or the other
+
+// Types:
+// Input-restricted: insertion at rear end, deletion at any end
+// Output-restricted: insertion at any end, deletion at rear end
+
+// Operations:
+// insert at front
+// insert at rear
+// delete from front
+// delete from rear
+
+// Applications: 
+// 1. Undo/redo operation in text editors
+// 2. Palindrome checker
+// 3. Multi-processor scheduling
